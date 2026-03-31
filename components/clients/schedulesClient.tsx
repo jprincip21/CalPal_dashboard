24c0b658ebaca1bd32079e223b885dcc715c5c75
jprincip21 <jprincip21@gmail.com>
2026-03-31 00:22:36 -0400

    Updated Client logic for create and deatils

--- components/clients/schedulesClient.tsx
+++ components/clients/schedulesClient.tsx
@@ -7,7 +7,6 @@ import ScheduleTable from "@/widgets/ScheduleTable";
 import ScheduleForm from "@/widgets/ScheduleForm";
 // import ScheduleDetail from "@/widgets/ScheduleDetail";
 import { Button } from "@/components/ui/button";
-import { Plus, ArrowLeft } from "lucide-react";
 
 type View = "table" | "create" | "detail";
 
@@ -24,13 +23,33 @@ export default function SchedulesClient() {
 
     return (
         <div className="flex flex-col gap-8 p-6 min-h-full rounded-xl bg-slate-50 shadow-sm">
-            <h1 className="text-3xl font-bold text-lavender-dark tracking-tight">Schedules</h1>
-            <ScheduleTable 
-                    schedules={schedules}
-                    onSelect={handleSelect}
-                    selectedId={selectedSchedule?.id}/>
-        </div>
-    )
-    
 
+            {/* Header */}
+            <div className="flex items-center justify-between">
+                <h1 className="text-3xl font-bold text-lavender-dark tracking-tight">Schedules</h1>
+                {view === "table" && (
+                    <Button type="button" onClick={() => setView("create")} className="flex items-center p-2 gap-2 h-11">Create Schedule</Button>
+                            )}
+                {(view === "create" || view === "detail") && (
+                    <Button type="button" 
+                        onClick={() => {
+                            setView("table");
+                            setSelectedSchedule(null);
+                            }} 
+                            className="flex items-center p-2 gap-2 h-11 bg-red-400 hover:bg-red-500">Back to Schedules</Button>
+                )}
+            </div>
+
+            {/* Body */}
+            <div className="bg-white rounded-lg shadow-sm border-t-4 border-lavender-primary p-6">
+                {view === "table" && (
+                    <ScheduleTable
+                        schedules={schedules}
+                        onSelect={handleSelect}
+                        selectedId={selectedSchedule?.id}
+                    />
+                )}
+            </div>
+        </div>
+    ) 
 }
