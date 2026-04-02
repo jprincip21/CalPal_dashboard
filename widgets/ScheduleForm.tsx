b18852cc9077d8f4d035ab2679432c9abd52c905
jprincip21 <jprincip21@gmail.com>
2026-04-01 22:08:36 -0400

    Fixed Bug causing scheduled weeks to be 6 days long

--- widgets/ScheduleForm.tsx
+++ widgets/ScheduleForm.tsx
@@ -28,7 +28,7 @@ export default function ScheduleForm({ locations, loading, onSubmit }: ScheduleF
     function getNextSaturday(sundayStr: string): string {
         const sunday = new Date(sundayStr);
         const saturday = new Date(sunday);
-        saturday.setDate(saturday.getDate() + 6)
+        saturday.setDate(saturday.getDate() + 7) // Had to update to 7 because 6 was making end day a friday
         return saturday.toLocaleDateString('en-CA');   
     };
 
