4f16e45638ad54b22a60d0ea5955ac61d1ed9386
jprincip21 <jprincip21@gmail.com>
2026-04-02 23:19:38 -0400

    Added Open/Close state

--- widgets/ShiftCell.tsx
+++ widgets/ShiftCell.tsx
@@ -27,34 +27,53 @@ export default function Shiftcell({
     disabled
 }: ShiftCellProps) {
     const [isOpen, setIsOpen] = useState(false)
+    
 
+if (isOpen) {
     return (
-    <td className="py-3 px-2 font-medium border-b border-slate-200">
-        <div className=" flex flex-col gap-1">
-            <p className="pt-1 text-slate-500 font-normal text-xs">Start Time:</p>
-            <Input 
-                type="time"
-                className="h-7 text-xs"
-            />
-            <p className="pt-1 text-slate-500 font-normal text-xs">End Time:</p>
-            <Input 
-                type="time"
-                className="h-7 text-xs"
-            />
-            <Button
-            type="button"
-            onClick={() => console.log("Create Shift")}
-            className="h-6 text-xs flex-1"
-            >
-                Save
-            </Button>
-            <Button
+    
+        <td className="py-3 px-2 font-medium border-b border-slate-200">
+            <div className=" flex flex-col gap-1">
+                <div className="flex justify-between">
+                <p className="pt-1 text-slate-500 font-normal text-xs">Start Time:</p>
+                <button 
+                    type="button"
+                    className="pt-1 text-slate-500 hover:text-slate-800 font-semibold text-xs"
+                    onClick={() => setIsOpen(false)}>
+                        X
+                </button>
+                </div>
+                <Input 
+                    type="time"
+                    className="h-7 text-xs"
+                />
+                <p className="pt-1 text-slate-500 font-normal text-xs">End Time:</p>
+                <Input 
+                    type="time"
+                    className="h-7 text-xs"
+                />
+                <Button
                 type="button"
-                onClick={() => console.log("Delete Shift")}
-                className="h-6 text-xs flex-1 bg-red-400 hover:bg-red-500"
-            >
-                Delete
-            </Button>
-        </div>
-    </td>)
+                onClick={() => console.log("Create Shift")}
+                className="h-6 text-xs flex-1"
+                >
+                    Save
+                </Button>
+                <Button
+                    type="button"
+                    onClick={() => console.log("Delete Shift")}
+                    className="h-6 text-xs flex-1 bg-red-400 hover:bg-red-500"
+                >
+                    Delete
+                </Button>
+                
+            </div>
+        </td>
+    
+    );
 }
+    return (
+        <td className="py-3 px-2 font-medium border-b border-slate-200" onClick={() => {setIsOpen(true)}}>
+            --
+        </td>
+    )}
